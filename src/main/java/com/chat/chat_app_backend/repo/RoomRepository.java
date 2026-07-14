3eddc55935121d75b2c6d1030d5b2c20c2139f6a
Prashant Negi <prashantnegi2288@gmail.com>
2026-07-14 12:14:59 +0530

    Added RoomRepository

---
+++ src/main/java/com/chat/chat_app_backend/repo/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.chat.chat_app_backend.repo;
+
+import com.chat.chat_app_backend.entities.Room;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RoomRepository extends MongoRepository<Room, String> {
+
+    // get room using room id
+    Room findByRoomId(String roomId);
+}
