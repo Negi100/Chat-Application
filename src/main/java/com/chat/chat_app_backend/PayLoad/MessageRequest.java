e56139c56bbebfb5dc2b80cd148ea5b96397b099
Prashant Negi <prashantnegi2288@gmail.com>
2026-07-15 14:33:55 +0530

    Implement payload package for Message Request

---
+++ src/main/java/com/chat/chat_app_backend/PayLoad/MessageRequest.java
@@ -0,0 +1,20 @@
+package com.chat.chat_app_backend.PayLoad;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageRequest {
+    private String content;
+    private String sender;
+    private String roomId;
+    private LocalDateTime messageTime;
+}
